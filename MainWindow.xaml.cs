ed8ec9706fa0812ef8d4c335c6e9d0f4e2018f97
elinest1105 <james.cobb812@gmail.com>
2024-05-02 14:45:24 -0400

    Implemented getting header of invoice

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
 
 namespace MindeeAPI_OCR
 {
@@ -59,15 +60,40 @@ namespace MindeeAPI_OCR
         private Invoice ParseInvoiceData(string jsonData)
         {
             var jsonObject = JsonConvert.DeserializeObject<dynamic>(jsonData);
-            Console.WriteLine(jsonObject.ToString());
-            return new Invoice
+            var document = jsonObject["document"];
+            var prediction = document["inference"]["prediction"];
+            Console.WriteLine(prediction);
+
+            var invoice = new Invoice
             {
-                Date = DateTime.Parse(jsonObject.date),
-                SupplierName = jsonObject.supplier,
-                InvoiceId = jsonObject.invoice_number,
-                TotalAmount = decimal.Parse(jsonObject.total),
-                IsIntegrated = false
+                InvoiceNumber = (string)prediction["invoice_number"]["value"],
+                Date = prediction["date"]["value"] != null ? DateTime.Parse((string)prediction["date"]["value"]) : null,
+                TotalAmount = prediction["total_amount"]["value"] != null ? decimal.Parse((string)prediction["total_amount"]["value"]) : 0,
+                SupplierName = (string)prediction["supplier_name"]["value"],
+                //Supplier = new Supplier
+                //{
+                //    Name = (string)prediction["supplier_name"]["value"],
+                //    Address = (string)prediction["supplier_address"]["value"],
+                //    Email = (string)prediction["supplier_email"]["value"],
+                //    PhoneNumber = (string)prediction["supplier_phone_number"]["value"]
+                //},
+                //Customer = new Customer
+                //{
+                //    Name = (string)prediction["customer_name"]["value"],
+                //    Address = (string)prediction["customer_address"]["value"],
+                //    Id = (string)prediction["customer_id"]["value"]
+                //},
+                //LineItems = ((JArray)prediction["line_items"]).Select(item => new LineItem
+                //{
+                //    Description = (string)item["description"],
+                //    ProductCode = (string)item["product_code"],
+                //    Quantity = (double)item["quantity"],
+                //    UnitPrice = (decimal)item["unit_price"],
+                //    TotalAmount = (decimal)item["total_amount"]
+                //}).ToList()
             };
+
+            return invoice;
         }
 
         private void LvInvoices_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -8,11 +8,37 @@ namespace MindeeAPI_OCR.Models
 {
     public class Invoice
     {
-        public DateTime Date { get; set; }
-        public string SupplierName {  get; set; }
-        public string InvoiceId { get; set; }
-        public Int32 TotalAmount { get; set;}
-        public Boolean IsIntegrated { get; set;}
+        public string InvoiceNumber { get; set; }
+        public DateTime? Date { get; set; }
+        public DateTime DueDate { get; set; }
+        public string SupplierName { get; set; }
+        public Supplier Supplier { get; set; }
+        public Customer Customer { get; set; }
+        public List<LineItem> LineItems { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
 
+    public class Supplier
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Id { get; set; }
+    }
+
+    public class LineItem
+    {
+        public string Description { get; set; }
+        public string ProductCode { get; set; }
+        public double Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalAmount { get; set; }
     }
 }
